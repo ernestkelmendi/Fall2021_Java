d14b642098677dfaa03c767580c2397ce3a4607d
ernestkelmendi <93290956+ernestkelmendi@users.noreply.github.com>
2021-11-02 18:13:50 -0400

    src/Homework1/File2.java

---
+++ src/Homework1/File2.java
@@ -0,0 +1,56 @@
+package Homework1;
+
+public class File2 {
+    public static void main(String[] args) {
+
+        // 1. Number of hours in a day
+
+        byte dayHours = 24;
+        System.out.println("Total number of hours in a day are " + dayHours);
+
+
+        // 2. Maximum number of days in a year
+
+        short yearDays = 366;
+        System.out.println("Total number of days in a year are " + yearDays);
+
+
+        // 3. Total number of employees at Marvel Entertainment
+
+        int employeeMarvel = 1196;
+        System.out.println("Total number of employees at Marvel Entertainment is " + employeeMarvel);
+
+
+        //4. Total number of population in Colombia
+
+        double populationColombia = 50.88;
+        System.out.println("The population of Colombia is " + populationColombia + " million");
+
+
+        //5. Mortgage Rate percentage at Bank of America
+
+        byte mortgageRatePercentage = 3;
+        System.out.println("The mortgage rate percentage at Bank of America is " + mortgageRatePercentage + "%");
+
+
+        //6. Total balance of Captain America at Berkshire Bank;
+
+        float bankBalance = 936.08f;
+        System.out.println("Total balance of Captain America at Berkshire Bank is " + "$"+ bankBalance);
+
+
+        //7. Does the sun rise from the West?
+
+        boolean sunRise = false;
+        System.out.println("Does the sun rise from the West?" + " = " + sunRise);
+
+
+        //8. Initials of your name, EK
+
+        char firstLetter = 'E';
+        char secondLetter = 'K';
+        System.out.println("Initials of my name are " + firstLetter + secondLetter);
+
+
+    }
+}

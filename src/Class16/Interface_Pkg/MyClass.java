2f2e89d4196a0b1326bddae747652da40843aae8
ernestkelmendi <93290956+ernestkelmendi@users.noreply.github.com>
2021-12-07 11:30:25 -0500

    src/Class9/Variable_Scope_Examples.java

---
+++ src/Class16/Interface_Pkg/MyClass.java
@@ -0,0 +1,7 @@
+package Class16.Interface_Pkg;
+
+public class MyClass {
+    public void sayHello() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Class17/LabSessionPkg/Users.java
@@ -0,0 +1,22 @@
+package Class17.LabSessionPkg;
+
+public class Users {
+    boolean isRaining;
+
+
+
+    public static void main(String[] args) {
+
+        ErnestMobiles am1 = new ErnestMobiles();
+        am1.makeCall();     // Calling...
+
+        Apple a1 = new Apple();
+        a1.makeCall();      // Apple Calling
+
+
+
+
+
+
+    }
+}
